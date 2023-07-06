e00c3236fc521bd1847e65e12ea176996dc5780a
Dthuong <duythuong300599@gmail.com>
2023-07-06 10:21:48 +0700

    feat: add api board

---
+++ src/components/molecules/Modal/modalAddBoard/index.tsx
@@ -0,0 +1,135 @@
+
+import styles from './index.module.scss'
+import { BackGround, IBackground } from 'utils/constants';
+import Svg from 'components/atoms/Svg';
+import skeleton from 'assets/images/skeletonBoard.svg'
+import check from 'assets/images/check.svg'
+import { Form } from 'antd';
+import { useEffect, useRef, useState } from 'react';
+import Input from 'components/atoms/Input';
+import Button from 'components/atoms/Button';
+import { addNewBoard } from 'api/boards';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from 'routes';
+
+interface Props {
+  open?: boolean
+}
+
+function ModalAddBoard(props: Props) {
+
+  type backgroundType = IBackground['value']
+
+  const [bgSelected, setBgSelected] = useState<backgroundType>('backGround1')
+  const [disableButton, setDisableButton] = useState(true);
+  const inputRef = useRef<HTMLInputElement>();
+
+  const [form] = Form.useForm()
+  const navigate = useNavigate()
+
+  const listBackground = Object.keys(BackGround)
+
+  const handleValuesChange = (value: any) => {
+    if (value.title.length >= 3) {
+      setDisableButton(false)
+    } else {
+      setDisableButton(true)
+    }
+  }
+
+  const handleFinish = async (value: any) => {
+    try {
+      const data = await form.validateFields(value)
+      const formData = {
+        title: data.title,
+        backGround: bgSelected
+      }
+
+      const res = await addNewBoard(formData)
+      if (res) {
+        navigate(ROUTES.BoardDetail(res._id))
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus()
+  }, [inputRef])
+
+
+  return (
+    <div className={styles.modalAddBoard}>
+      <header className={styles.header}>Create board</header>
+      <div className={styles.preview}>
+        <div
+          className={styles.item}
+          style={{
+            background: BackGround[bgSelected]
+          }}
+        >
+          <div className={styles.skeleton}>
+            <Svg src={skeleton} alt='skeleton' />
+          </div>
+        </div>
+      </div>
+      <div className={styles.background}>
+        <h3>Background</h3>
+        <ul className={styles.listBackground}>
+          {listBackground.map((bg: any, i: number) => {
+            const data: backgroundType = bg;
+            return (
+              <li key={i} >
+                <button
+                  className={styles.item}
+                  onClick={() => setBgSelected(bg)}
+                  style={{
+                    background: BackGround[data]
+                  }}
+                >
+                  {bgSelected === bg &&
+                    <span className={styles.selected}>
+                      <Svg src={check} alt='check' />
+                    </span>
+                  }
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <Form
+        form={form}
+        layout='vertical'
+        onValuesChange={handleValuesChange}
+        onFinish={handleFinish}
+      >
+        <Form.Item
+          className={styles.title}
+          name='title'
+          label='Board title'
+          rules={[
+            { required: true, message: 'Board title is required' },
+            { min: 3, max: 20, message: 'Board title must be range 3 to 20 character' }
+          ]}
+        >
+          <Input ref={inputRef} minLength={3} maxLength={20} />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            className={styles.button}
+            type='primary'
+            htmlType='submit'
+            disabled={disableButton}
+          >
+            Create
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
+}
+
+export default ModalAddBoard
--- src/components/pages/listBoard/index.tsx
+++ src/components/pages/listBoard/index.tsx
@@ -1,15 +1,35 @@
 import { useListBoards } from 'hooks/useBoards'
 import styles from './index.module.scss'
-import { Spin } from 'antd';
+import { Popover, Spin } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from 'routes';
 import { BackGround } from 'utils/constants';
+import classNames from 'classnames';
+import ModalAddBoard from 'components/molecules/Modal/modalAddBoard';
+import iconDel from 'assets/images/DeleteOutlinedWhite.svg'
+import iconWarning from 'assets/images/warning.svg'
+import Svg from 'components/atoms/Svg';
+import { useState } from 'react';
+import Modal from 'components/atoms/Modal';
+import { deleteBoard } from 'api/boards';
 
 function ListBoard() {
-
   const navigate = useNavigate();
-  const { data, isLoading } = useListBoards();
+  const { data, isLoading, refetch } = useListBoards();
+  const [showModalConfirm, setShowModalConfirm] = useState(false)
+  const [board, setBoard] = useState<any>()
 
+  const handleDeleteBoard = async () => {
+    try {
+      const res = await deleteBoard(board._id);
+      if (res.success) {
+        refetch()
+        setShowModalConfirm(false)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <Spin spinning={isLoading} >
@@ -25,11 +45,50 @@ function ListBoard() {
               className={styles.boardCard}
               onClick={() => navigate(ROUTES.BoardDetail(board._id))}
             >
+              <div className={styles.mask}>
+                <Svg
+                  className={styles.delete}
+                  src={iconDel}
+                  width={24}
+                  height={24}
+                  onClick={(e) => {
+                    setShowModalConfirm(true)
+                    setBoard(board)
+                    e.stopPropagation();
+                  }}
+                />
+              </div>
               <h2 className={styles.boardTitle}>{board.title}</h2>
             </div>
           )
         })}
+        <Popover
+          arrow={false}
+          content={<ModalAddBoard open />}
+          placement='rightTop'
+          trigger='click'
+        >
+          <div
+            className={classNames(styles.boardCard, styles.createBoard)}
+          >
+            <h2 className={styles.title}>Create new board</h2>
+          </div>
+        </Popover>
       </div>
+      {showModalConfirm &&
+        <Modal
+          width={360}
+          centered
+          open={showModalConfirm}
+          titleModal={`Are you sure delete "${board.title}"?`}
+          okText='Delete'
+          okType='danger'
+          icon={iconWarning}
+          onOk={handleDeleteBoard}
+          onCancel={() => setShowModalConfirm(false)}
+          closable={false}
+        />
+      }
     </Spin>
   )
 }
